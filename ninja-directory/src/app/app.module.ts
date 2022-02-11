04e1f09323d173801fbb8fad46fa79c136537045
Kehinde Enigbokan <kehinde.enigbokan@gmail.com>
2022-02-11 11:01:32 +0100

    ii

--- ninja-directory/src/app/app.module.ts
+++ ninja-directory/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { DirectoryComponent } from './directory/directory.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DirectoryComponent
   ],
 
   imports: [
